fe960ffa4bef5ce87cb82a6809f12127caca5b21
Jiri Hnidek <jhnidek@redhat.com>
2026-02-17 22:25:42 +0100

    feat: WIP: Added more "rhc configure" features
    * Card ID: CCT-1808
    * It is possible to disable/enable features
    * It is possible to show status of features
    * When the feature is enabled/disabled before registration,
      then preference is only written to preference file:
      /var/lib/rhc/rhc-connect-features-prefs.json
    * When the feature is enabled/disabled after registration,
      then related feature is started or stopped
    * Some UI improvements
    
    Signed-off-by: Jiri Hnidek <jhnidek@redhat.com>

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -10,10 +10,12 @@ import (
 )
 
 const (
-	colorGreen  = "\u001B[32m"
-	colorYellow = "\u001B[33m"
-	colorRed    = "\u001B[31m"
-	colorReset  = "\u001B[0m"
+	ColorGreen  = "\x1b[0032m"
+	ColorYellow = "\x1b[0033m"
+	ColorRed    = "\x1b[0031m"
+	ColorGrey   = "\x1b[0090m"
+	DefaultText = "\x1b[0039m"
+	ColorReset  = "\x1b[0000m"
 )
 
 var Indent = indent{
@@ -27,9 +29,11 @@ type indent struct {
 }
 
 type icons struct {
-	Ok    string
-	Info  string
-	Error string
+	Ok       string
+	Info     string
+	Error    string
+	Enabled  string
+	Disabled string
 }
 
 var Icons icons
@@ -66,14 +70,18 @@ func ConfigureOutput(rich bool, colored bool, machine bool) {
 	}
 
 	Icons = icons{
-		Ok:    "✓",
-		Info:  "●",
-		Error: "𐄂",
+		Ok:       "✓",
+		Info:     "●",
+		Error:    "𐄂",
+		Enabled:  "✓",
+		Disabled: "x",
 	}
 	if rich && colored {
-		Icons.Ok = colorGreen + Icons.Ok + colorReset
-		Icons.Info = colorYellow + Icons.Info + colorReset
-		Icons.Error = colorRed + Icons.Error + colorReset
+		Icons.Ok = ColorGreen + Icons.Ok + ColorReset
+		Icons.Info = ColorYellow + Icons.Info + ColorReset
+		Icons.Error = ColorRed + Icons.Error + ColorReset
+		Icons.Enabled = ColorGreen + Icons.Enabled + ColorReset
+		Icons.Disabled = ColorRed + Icons.Disabled + ColorReset
 	}
 }
 
